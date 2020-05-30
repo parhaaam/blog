aa43e008c8467fe1d98444e9bbd1ee2e6cba9db6
panian@outlook.com <parnian@outlook.com>
2020-05-30 10:33:51 +0430

    Laravel Passport Installed

--- app/User.php
+++ app/User.php
@@ -5,12 +5,13 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 use App\Comment;
 use App\Category;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+     use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware'=> ['auth'],'prefix' => 'admin'],function () {
+  Route::get('/categories','CategoryController@index')->name('catList')->middleware('can:viewAny,App\Category');
+  Route::get('/category','CategoryController@create')->name('createCat')->middleware('can:create,App\Category');
+  Route::post('/category','CategoryController@store')->name('storeCat')->middleware('can:create,App\Category');
+  Route::get('/category/{category}','CategoryController@edit')->name('editCat')->middleware('can:update,category');
+  Route::put('/category/{category}','CategoryController@update')->name('updateCat')->middleware('can:update,category');
+  Route::delete('/category/{category}','CategoryController@destroy')->name('deleteCat')->middleware('can:delete,category');
+});
+
+Route::get('password-grant-auth', function (Request $request) {
+
+
+    $http = new GuzzleHttp\Client;
+    // Make call to "Tweeter," our Passport-powered OAuth server
+    $response = $http->post('http://127.0.0.1:8002/oauth/token', [
+        'form_params' => [
+            'grant_type'    => 'password',
+            'client_id'     => 6,
+            'client_secret' => 'zvanzs1qCPjXTYnZvIS8e6LGUsWsOQh5CDVtf7gw',
+            'username'      => $request->input('username'),
+            'password'      => $request->input('password'),
+            'scope'         => '',
+            ],
+          ]);
+
+    return $thisUsersTokens = json_decode((string) $response->getBody(), true);
+    // Do stuff with the tokens
+});
